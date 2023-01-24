425c14af1a023d6c81baad21378af1ef6bb2bdd1
ChadsLaptop <chadjcampbell@gmail.com>
2023-01-23 22:39:21 -0500

    remove boilerplate and add starting routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
+import Root from "./routes/Root";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/Root.tsx
@@ -0,0 +1,5 @@
+function Root() {
+  return <h1>Home</h1>;
+}
+
+export default Root;
