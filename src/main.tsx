0f75123452e30e4cd174b006e72379ff71ae50ad
Chad <chadjcampbell@gmail.com>
2023-01-27 11:17:54 -0500

    remove another unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import React, { Children } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Collection from "./routes/Collection";
