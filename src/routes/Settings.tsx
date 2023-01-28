a02b8bb0caed9c717368ac3bbfbc366d3258a946
Chad <chadjcampbell@gmail.com>
2023-01-27 20:05:36 -0500

    add favicon

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,5 +1,15 @@
 const Settings = () => {
-  return <h1>Settings</h1>;
+  return (
+    <>
+      <h1>Settings</h1>
+      <a
+        href="https://www.flaticon.com/free-icons/chat-box"
+        title="chat box icons"
+      >
+        Chat box icons created by Ilham Fitrotul Hayat - Flaticon
+      </a>
+    </>
+  );
 };
 
 export default Settings;
