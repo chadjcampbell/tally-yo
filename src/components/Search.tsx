32ddce0b93d0dd26b908cfcbe90044cfa0814f0b
Chad <chadjcampbell@gmail.com>
2023-01-31 20:08:22 -0500

    layout Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,27 @@
-import { Input } from "@chakra-ui/react";
+import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Search = () => {
-  return <Input placeholder="Search..." />;
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSearch = () => {};
+
+  return (
+    <InputGroup>
+      {" "}
+      <Input
+        placeholder="Search..."
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <InputRightElement width="4.5rem">
+        <Button mr="1" colorScheme="teal" size="sm" onClick={handleSearch}>
+          Search
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
 };
 
 export default Search;
