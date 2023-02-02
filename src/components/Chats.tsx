5a945499d010562c15d6ec64510aaf44ae1b4329
ChadsLaptop <chadjcampbell@gmail.com>
2023-02-01 21:28:33 -0500

    add Chats(friends list) snapshot, update realtime

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,39 +1,57 @@
 import { VStack, HStack, Avatar, Text, Heading } from "@chakra-ui/react";
-import { useState } from "react";
+import { onSnapshot, doc, DocumentData } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { db } from "../firebase";
 
 const Chats = () => {
-  const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState<DocumentData | undefined>([]);
+  const { user } = useContext(AuthContext);
+
+  user &&
+    useEffect(() => {
+      const unsub = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
+        setChats(doc.data());
+        console.log("Current data: ", doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    }, [user.uid]);
 
   return (
     <>
       <Heading color="teal.800" as="h2" size="lg" mt="12">
         Tally Friends
       </Heading>
-      <VStack
-        display={{ md: "flex" }}
-        alignItems="flex-start"
-        spacing="1px"
-        mt="2"
-        border="1px solid teal"
-        borderRadius="md"
-        _hover={{
-          bg: "blue.500",
-          color: " white",
-          cursor: "pointer",
-        }}
-      >
-        <HStack m="1">
-          <Avatar size={"md"} src={"" || undefined} />
-          <VStack
-            display={{ md: "flex" }}
-            alignItems="flex-start"
-            spacing="1"
-            ml="2"
-          >
-            <Text fontSize="md">{""}</Text>
-          </VStack>
-        </HStack>
-      </VStack>
+      {Object.entries(chats!).map((chat) => (
+        <VStack
+          key={chat[1].userInfo.uid}
+          display={{ md: "flex" }}
+          alignItems="flex-start"
+          spacing="1px"
+          mt="2"
+          bgColor="teal.200"
+          borderRadius="md"
+          _hover={{
+            bg: "teal.500",
+            color: " white",
+            cursor: "pointer",
+          }}
+        >
+          <HStack m="1">
+            <Avatar size={"md"} src={chat[1].userInfo.photoURL} />
+            <VStack
+              display={{ md: "flex" }}
+              alignItems="flex-start"
+              spacing="1"
+              ml="2"
+            >
+              <Text fontSize="md">{chat[1].userInfo.displayName}</Text>
+            </VStack>
+          </HStack>
+        </VStack>
+      ))}
     </>
   );
 };
