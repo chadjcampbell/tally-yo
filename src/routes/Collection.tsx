5ac5178d02009a01a3631562d617290389f3a699
Chad <chadjcampbell@gmail.com>
2023-02-06 17:17:26 -0500

    start cards on collection & settings

--- src/routes/Collection.tsx
+++ src/routes/Collection.tsx
@@ -1,5 +1,24 @@
+import {
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  CardFooter,
+} from "@chakra-ui/react";
+
 const Collection = () => {
-  return <h1>Collection</h1>;
+  return (
+    <Card mt="3" borderRadius="1em" align="center" width="full" height="full">
+      <CardHeader
+        borderRadius="1em 1em 0 0"
+        backgroundColor="teal.200"
+        width="full"
+      >
+        <Heading size="md">Collection</Heading>
+      </CardHeader>
+      <CardBody minHeight="75vh" width="full"></CardBody>
+    </Card>
+  );
 };
 
 export default Collection;
--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,14 +1,32 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+} from "@chakra-ui/react";
+
 const Settings = () => {
   return (
-    <>
-      <h1>Settings</h1>
-      <a
-        href="https://www.flaticon.com/free-icons/chat-box"
-        title="chat box icons"
+    <Card mt="3" borderRadius="1em" align="center" width="full" height="full">
+      <CardHeader
+        borderRadius="1em 1em 0 0"
+        backgroundColor="teal.200"
+        width="full"
       >
-        Chat box icons created by Ilham Fitrotul Hayat - Flaticon
-      </a>
-    </>
+        <Heading size="md">Settings</Heading>
+      </CardHeader>
+      <CardBody minHeight="65vh" width="full"></CardBody>
+      <CardFooter>
+        {" "}
+        <a
+          href="https://www.flaticon.com/free-icons/chat-box"
+          title="chat box icons"
+        >
+          Chat box icons created by Ilham Fitrotul Hayat - Flaticon
+        </a>{" "}
+      </CardFooter>
+    </Card>
   );
 };
 
