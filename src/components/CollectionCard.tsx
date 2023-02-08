b74794507ddd935522774fb7f81d25ebe1aa94a7
Chad <chadjcampbell@gmail.com>
2023-02-08 18:30:45 -0500

    add modal to each item

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -10,22 +10,36 @@ import {
   Image,
   HStack,
   Box,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  VStack,
+  useToast,
 } from "@chakra-ui/react";
 import { DocumentData } from "firebase/firestore";
 import { CollectionItem } from "../routes/Collection";
 import { titleCase } from "../utils/titlecase";
-import { CollectionModal } from "./CollectionModal";
 
-export type CollectionCardProps = {
+type CollectionCardProps = {
   item: CollectionItem;
   userStore: DocumentData | undefined;
 };
 
 export const CollectionCard = ({ item, userStore }: CollectionCardProps) => {
+  const { onOpen, isOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  const handleBuy = () => {};
+  const handleSell = () => {};
+
   return (
     <Card align="center" justify="center" shadow="lg">
       <CardBody>
-        <CollectionModal item={item} userStore={userStore} />
         <Image src={item.image_uri} alt={item["file-name"]} borderRadius="lg" />
         <Stack mt="6" spacing="3">
           <Heading size="md">{titleCase(item.name["name-USen"])}</Heading>
@@ -38,9 +52,42 @@ export const CollectionCard = ({ item, userStore }: CollectionCardProps) => {
       </CardBody>
       <Divider />
       <CardFooter>
-        <Button variant="solid" colorScheme="teal">
+        <Button onClick={onOpen} variant="solid" colorScheme="teal">
           More Info
         </Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{titleCase(item.name["name-USen"])}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                {" "}
+                <Image
+                  src={item.image_uri}
+                  alt={item["file-name"]}
+                  borderRadius="lg"
+                />
+                <Text>{item["museum-phrase"]}</Text>
+                <HStack>
+                  <Image src="/coin.png" />{" "}
+                  <Text color="blue.600" fontSize="2xl">
+                    {item.price}
+                  </Text>
+                </HStack>
+              </VStack>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button onClick={handleBuy} colorScheme="blue" mr={3}>
+                Buy Now
+              </Button>
+              <Button onClick={handleSell} variant="outline">
+                Sell
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </CardFooter>
     </Card>
   );
