69853bef397382a31d21608ce4774d9f1f4f8b62
Chad <chadjcampbell@gmail.com>
2023-02-11 19:03:53 -0500

    update styling on login and registration

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -7,13 +7,14 @@ import {
   InputLeftElement,
   InputRightElement,
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { FaImage } from "react-icons/fa";
 
 const CFaImage = chakra(FaImage);
 
 export const FileInput = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [iconColor, setIconColor] = useState("gray.300");
   const fileStatus = "add an avatar";
 
   return (
@@ -22,9 +23,10 @@ export const FileInput = () => {
         <InputLeftElement
           pointerEvents="none"
           color="gray.300"
-          children={<CFaImage color="gray.300" />}
+          children={<CFaImage color={iconColor} />}
         />
         <input
+          onInput={() => setIconColor("teal.400")}
           id="file"
           type="file"
           accept="image/*"
@@ -33,7 +35,12 @@ export const FileInput = () => {
         />
         <Input border="none" placeholder={fileStatus} readOnly type="text" />
         <InputRightElement width="8rem">
-          <Button onClick={() => inputRef.current?.click()} id="avatar">
+          <Button
+            onClick={() => inputRef.current?.click()}
+            id="avatar"
+            border="md"
+            borderColor={iconColor}
+          >
             Upload Avatar
           </Button>
         </InputRightElement>
