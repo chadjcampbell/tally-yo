e95d1b8b532b1bd73da5673cb8dd8bf96d91aad6
Chad <chadjcampbell@gmail.com>
2023-02-28 18:11:52 -0500

    more work on api

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -7,7 +7,7 @@ const Buy = () => {
     e.preventDefault();
     const searchStock = e.currentTarget.stockName.value;
     let result = await fetch(
-      `https://cloud.iexapis.com/stable/stock/twtr/quote/&token=${API_KEY}`
+      `https://cloud.iexapis.com/stable/stock/${searchStock}/quote?displayPercent=true&token=${API_KEY}`
     );
     console.log(result);
   };
