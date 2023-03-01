be8ac5d61a7d12c43009ac329c311628fb66e676
Chad <chadjcampbell@gmail.com>
2023-02-28 20:41:21 -0500

    switch stock API again, to Alpaca, fix type error

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -1,14 +1,23 @@
 import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { AlpacaClient } from "@master-chief/alpaca";
+import { FormEvent } from "react";
 
 const Buy = () => {
-  const API_KEY = import.meta.env.VITE_API_KEY;
+  const API_KEY = import.meta.env.VITE_KEY;
+  const API_SECRET = import.meta.env.VITE_SECRET;
+  const client = new AlpacaClient({
+    credentials: {
+      key: API_KEY,
+      secret: API_SECRET,
+      paper: true,
+    },
+    rate_limit: true,
+  });
 
-  const handleSearch = async (e) => {
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const searchStock = e.currentTarget.stockName.value;
-    let result = await fetch(
-      `https://cloud.iexapis.com/stable/stock/${searchStock}/quote?displayPercent=true&token=${API_KEY}`
-    );
+    let result = await client.getLatestTrade({ symbol: `${searchStock}` });
     console.log(result);
   };
 
