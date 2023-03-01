a6d3c14ab66c531808bbf1ccd0f41ef5f795ef10
Chad <chadjcampbell@gmail.com>
2023-03-01 13:49:18 -0500

    set up axios for trending stocks api call

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -1,19 +1,45 @@
 import {
   Button,
   Card,
+  Text,
   Flex,
   FormControl,
   FormHelperText,
   FormLabel,
   HStack,
   Input,
+  VStack,
+  CardHeader,
+  CardBody,
 } from "@chakra-ui/react";
 import { AlpacaClient } from "@master-chief/alpaca";
-import { FormEvent } from "react";
+import axios from "axios";
+import { FormEvent, useEffect, useState } from "react";
+import { trendingBackup } from "../utils/backupTrending";
+import Loading from "./Loading";
+
+type TrendingStocks = {
+  finance: {
+    result: {
+      count: number;
+      quotes: {
+        symbol: string;
+      }[];
+      jobTimestamp: number;
+      startInterval: number;
+    }[];
+    error: null;
+  };
+};
 
 const Buy = () => {
   const API_KEY = import.meta.env.VITE_KEY;
   const API_SECRET = import.meta.env.VITE_SECRET;
+  const TRENDING_KEY = import.meta.env.VITE_TRENDING;
+
+  const [trending, setTrending] = useState<TrendingStocks | null>(null);
+  const [loading, setLoading] = useState(true);
+
   const client = new AlpacaClient({
     credentials: {
       key: API_KEY,
@@ -23,6 +49,35 @@ const Buy = () => {
     rate_limit: true,
   });
 
+  false &&
+    useEffect(() => {
+      if (trending === null) {
+        const options = {
+          method: "GET",
+          url: "https://yfapi.net/v1/finance/trending/US",
+          params: { modules: "defaultKeyStatistics,assetProfile" },
+          headers: {
+            "x-api-key": TRENDING_KEY,
+          },
+        };
+        axios
+          .request(options)
+          .then(function (response) {
+            console.log(response.data);
+            setTrending(response.data);
+            setLoading(false);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      }
+    }, []);
+
+  useEffect(() => {
+    setTrending(trendingBackup);
+    setLoading(false);
+  }, []);
+
   const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const searchStock = e.currentTarget.stockName.value;
@@ -31,24 +86,43 @@ const Buy = () => {
   };
 
   return (
-    <Flex align="center" justify="center">
-      <Card p="3" shadow="xl">
-        <form onSubmit={(e) => handleSearch(e)}>
-          <FormControl>
-            <FormLabel textAlign="center">Search for a stock</FormLabel>
-            <HStack>
-              <Input id="stockName" max="10" type="text" />
+    <VStack>
+      <Flex align="center" justify="center" mb="5">
+        <Card p="3" shadow="xl">
+          <form onSubmit={(e) => handleSearch(e)}>
+            <FormControl>
+              <FormLabel textAlign="center">Search for a stock</FormLabel>
+              <HStack>
+                <Input id="stockName" max="10" type="text" />
 
-              <Button colorScheme="teal" type="submit">
-                Search
-              </Button>
-            </HStack>
+                <Button colorScheme="teal" type="submit">
+                  Search
+                </Button>
+              </HStack>
 
-            <FormHelperText textAlign="center">e.g. 'twtr' </FormHelperText>
-          </FormControl>
-        </form>
-      </Card>
-    </Flex>
+              <FormHelperText textAlign="center">e.g. 'twtr' </FormHelperText>
+            </FormControl>
+          </form>
+        </Card>
+      </Flex>
+      <Text fontSize="2xl" as="u">
+        Trending Stocks
+      </Text>
+      <Flex h="auto">
+        {loading ? (
+          <Loading />
+        ) : (
+          trending?.finance.result[0].quotes.map((stock) => (
+            <Card key={stock.symbol}>
+              <CardHeader>{stock.symbol}</CardHeader>
+              <CardBody>
+                <Button>More Info</Button>
+              </CardBody>
+            </Card>
+          ))
+        )}
+      </Flex>
+    </VStack>
   );
 };
 
---
+++ src/utils/backupTrending.ts
@@ -0,0 +1,74 @@
+export const trendingBackup = {
+  finance: {
+    result: [
+      {
+        count: 20,
+        quotes: [
+          {
+            symbol: "RIVN",
+          },
+          {
+            symbol: "RETA",
+          },
+          {
+            symbol: "LOW",
+          },
+          {
+            symbol: "NIO",
+          },
+          {
+            symbol: "NVAX",
+          },
+          {
+            symbol: "FSLR",
+          },
+          {
+            symbol: "ONCS",
+          },
+          {
+            symbol: "SRPT",
+          },
+          {
+            symbol: "RY.TO",
+          },
+          {
+            symbol: "DUOL",
+          },
+          {
+            symbol: "MQ",
+          },
+          {
+            symbol: "HPX",
+          },
+          {
+            symbol: "LHDX",
+          },
+          {
+            symbol: "IQ",
+          },
+          {
+            symbol: "CPNG",
+          },
+          {
+            symbol: "NVTA",
+          },
+          {
+            symbol: "HUBC",
+          },
+          {
+            symbol: "BGS",
+          },
+          {
+            symbol: "HLBZ",
+          },
+          {
+            symbol: "FHN",
+          },
+        ],
+        jobTimestamp: 1677689829534,
+        startInterval: 202303011600,
+      },
+    ],
+    error: null,
+  },
+};
