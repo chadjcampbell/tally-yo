b0cf03f00ae3dac5236a7edb447b857e590ba264
ChadsLaptop <chadjcampbell@gmail.com>
2023-03-08 10:00:07 -0500

    start stock info modal

--- src/components/StockCard.tsx
+++ src/components/StockCard.tsx
@@ -7,6 +7,14 @@ import {
   Text,
   HStack,
   Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { YFStockData } from "../types/YFStockData";
 import { stringToColor } from "../utils/stringToColor";
@@ -17,37 +25,56 @@ type StockCardProps = {
 };
 
 const StockCard = ({ stock }: StockCardProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Card w="250px" h="250px" shadow="lg" m="3" p="3">
-      <CardHeader>
-        <VStack align="left">
-          <Text
-            borderRadius="lg"
-            pl="1"
-            pr="1"
-            w="fit-content"
-            bgColor={stringToColor(stock.symbol)}
-          >
-            {" "}
-            {stock.symbol}
-          </Text>
-          <Text> {stock.shortName}</Text>
-        </VStack>
-      </CardHeader>
-      <CardBody mx="auto">
-        <HStack display="flex" alignItems="space-between">
+    <>
+      <Card w="250px" h="250px" shadow="lg" m="3" p="3">
+        <CardHeader>
           <VStack align="left">
-            <Text pl="1">${stock.regularMarketPrice.toFixed(2)}</Text>
-            <PercentBox percent={stock.regularMarketChangePercent} />
+            <Text
+              borderRadius="lg"
+              pl="1"
+              pr="1"
+              w="fit-content"
+              bgColor={stringToColor(stock.symbol)}
+            >
+              {" "}
+              {stock.symbol}
+            </Text>
+            <Text> {stock.shortName}</Text>
           </VStack>
-          <Flex alignItems="end">
-            <Button alignSelf="bottom" colorScheme="teal">
-              Info
+        </CardHeader>
+        <CardBody mx="auto">
+          <HStack display="flex" alignItems="space-between">
+            <VStack align="left">
+              <Text pl="1">${stock.regularMarketPrice.toFixed(2)}</Text>
+              <PercentBox percent={stock.regularMarketChangePercent} />
+            </VStack>
+            <Flex alignItems="end">
+              <Button onClick={onOpen} alignSelf="bottom" colorScheme="teal">
+                Info
+              </Button>
+            </Flex>
+          </HStack>
+        </CardBody>
+      </Card>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{stock.longName}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>This is a test</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
             </Button>
-          </Flex>
-        </HStack>
-      </CardBody>
-    </Card>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
