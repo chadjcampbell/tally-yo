ac4440ddf852a785eeb6c157e52b0669d6eb0322
Chad <chadjcampbell@gmail.com>
2023-03-10 13:27:12 -0500

    add all data and dynamic graph colors, rm faker

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -10,7 +10,6 @@ import {
   Legend,
 } from "chart.js";
 import { Chart } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
 import { YFStockData } from "../types/YFStockData";
 import { YFChartData } from "../types/YFChartData";
 import { HStack, VStack, Text } from "@chakra-ui/react";
@@ -24,35 +23,6 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-
-const colors = [
-  "red",
-  "orange",
-  "yellow",
-  "lime",
-  "green",
-  "teal",
-  "blue",
-  "purple",
-];
-
-function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea) {
-  const colorStart = faker.helpers.arrayElement(colors);
-  const colorMid = faker.helpers.arrayElement(
-    colors.filter((color) => color !== colorStart)
-  );
-  const colorEnd = faker.helpers.arrayElement(
-    colors.filter((color) => color !== colorStart && color !== colorMid)
-  );
-
-  const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
-
-  gradient.addColorStop(0, colorStart);
-  gradient.addColorStop(0.5, colorMid);
-  gradient.addColorStop(1, colorEnd);
-
-  return gradient;
-}
 
 type ChartComponentProps = {
   stock: YFStockData;
@@ -74,6 +44,21 @@ export const ChartComponent = ({
       chartDatafromAPI.indicators.adjclose[0].adjclose.length - 1
     ];
   const thirtyDayPercent = ((v2 - v1) / Math.abs(v1)) * 100;
+  const stockUp = thirtyDayPercent > 0 ? true : false;
+
+  function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea) {
+    const colorStart = stockUp ? "#80ff80" : "#ff8080";
+    const colorMid = stockUp ? "#00ff00" : "#ff0000";
+    const colorEnd = stockUp ? "#008000" : "#800000";
+
+    const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+    gradient.addColorStop(0, colorStart);
+    gradient.addColorStop(0.5, colorMid);
+    gradient.addColorStop(1, colorEnd);
+
+    return gradient;
+  }
 
   const labels = chartDatafromAPI.timestamp.map((ts) =>
     new Date(ts * 1000).toLocaleDateString().slice(0, -5)
@@ -115,7 +100,7 @@ export const ChartComponent = ({
       <VStack>
         <Text>Current: ${stock.regularMarketPrice.toFixed(2)}</Text>
         <HStack>
-          <Text>30 day % change</Text>
+          <Text>30 day change:</Text>
           <PercentBox percent={thirtyDayPercent} />
         </HStack>
       </VStack>
